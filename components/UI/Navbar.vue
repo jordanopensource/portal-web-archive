e9b3a76b42abb0ad9401d6b9fde1d92805c0db6c
thread-koder <mohamad.fberjawi@gmail.com>
2021-11-24 18:23:01 +0200

    Create the nav bar component

---
+++ components/UI/Navbar.vue
@@ -0,0 +1,38 @@
+<template>
+  <nav>
+    <!-- Main Menu -->
+    <menuItems
+      :menu="mainMenu"
+      menu-item-class="nav-link"
+      class="inline-block"
+    />
+    <!-- Language Switcher -->
+    <languageSwitcher />
+  </nav>
+</template>
+
+<script>
+import menuItems from '@/components/Menu/MenuItems'
+import languageSwitcher from '@/components/UI/LanguageSwitcher'
+export default {
+  name: 'Navbar',
+  components: {
+    menuItems,
+    languageSwitcher,
+  },
+  computed: {
+    mainMenu() {
+      return this.$store.getters.loadedMenus.mainMenu
+    },
+  },
+}
+</script>
+
+<style lang="postcss" scoped>
+.lang {
+  @apply text-sm opacity-50;
+}
+.lang.nuxt-link-active {
+  @apply opacity-100;
+}
+</style>
