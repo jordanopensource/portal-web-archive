59cc5aefc7156b7a05a622f782c707b4b4d8ceb1
thread-koder <mohamad.fberjawi@gmail.com>
2021-11-24 17:23:03 +0200

    Add the arabic lang file

---
+++ lang/ar.js
@@ -0,0 +1,207 @@
+export default {
+  pages: {
+    becomeAMember: 'انضم إلينا',
+    home: 'الصفحة الرئيسية',
+    blog: 'المدونة',
+    events: 'الفعاليات',
+  },
+  // Blog
+  blog: {
+    all: 'جميع المقالات',
+    latest: 'آخر المقالات',
+    more: 'المزيد من المقالات',
+    featured: 'مقالات مختارة',
+    spotlight: 'آخر المقالات',
+    disclaimer: 'الآراء المنشورة تعبر عن رأي صاحبها ولا تعبر بالضرورة عن رأي الجمعية الأردنية للمصدر المفتوح (جوسا)'
+  },
+  // Time Card
+  timeCard: {
+    title: 'التاريخ والوقت',
+    from: 'من',
+    to: 'إلى',
+    gmt: 'توقيت غرينتش',
+  },
+  // Location Card
+  locationCard: {
+    title: 'الموقع'
+  },
+  // Event
+  events: {
+    upcoming: 'الفعاليات القادمة',
+    previous: 'الفعاليات السابقة',
+    more: 'جميع فعاليتنا',
+    noUpcoming: 'لا توجد فعاليات قادمة',
+    noPrevious: 'لا توجد فعاليات سابقة',
+    register: 'التسجيل',
+    registerDesc: 'سجل هنا إذا كنت ترغب في حضور هذه الفعالية:',
+    registerSuccess: 'شكرًا لك, تم التسجيل لهذه الفعالية بنجاح!',
+    registerFail: 'هنالك خطأ, الرجاء المحاولة لاحقًا!',
+    registerExist: 'لقد سجلت مسبقًا لهذه الفعالية!',
+    join: 'انضم إلى هذه الفعالية',
+    hostedBy: 'بتنظيم من الجمعية الأردنية للمصدر المفتوح',
+    happening: 'يحدث الآن',
+    joinCall: 'هذه الفعالبة تحدث عبر الإنترنت. يمكنك المشاركة عن طريق تعبئة النموذج التالي',
+    notRegistered: 'إذا لم تسجل في هذه الفالية, الرجاء التسجيل',
+    here: 'هنا',
+    onlineEvent: 'عبر الإنترنت'
+  },
+  eventCats: {
+    all: 'جميع الفعاليات'
+  },
+  // Contact Us
+  contact: {
+    address: 'العنوان',
+    workingHours: 'ساعات العمل',
+    voice: 'هاتف',
+    email: 'البريد الإلكتروني',
+    connect: 'تواصل معنا',
+    comma: '،'
+  },
+  // Feedback
+  feedback: {
+    title: 'الاقتراحات والاستفسارات',
+    description: 'سواء أكنت راضيًا أم غير راضٍ عن عملنا أو نشاطاتنا، فإننا نضع دومًا اقتراحاتك وتعليقاتك محل التقدير والاهتمام.',
+    fillForm: 'إذا كان لديك أي تعليق أو شكوى، فإننا ندعوك إلى تعبئة النموذج أدناه.',
+    form: {
+      subject: 'ما موضوع اقتراحك أو استفسارك؟',
+      inquiry: 'نرجو إضافة أي تفاصيل عنه',
+      name: 'اسمك',
+      email: 'بريدك الإلكتروني',
+      submit: 'إرسال'
+    }
+  },
+  // Publications
+  publication: {
+    title: 'الأبحاث والمنشورات',
+    publishDate: 'تاريخ النشر:',
+    featured: 'منشور مختار',
+    all: 'جميع الأبحاث والمنشورات'
+  },
+  publicationCats: {
+    title: {
+      all: 'جميع الأبحاث والمنشورات'
+    }
+  },
+  // Board & Staff
+  boardStaff: {
+    board: 'هيئة الإدارة',
+    staff: 'الفريق العامل',
+    previousBoard: 'أعضاء هيئة الإدارة السابقون',
+    previousStaff: 'أعضاء الفريق السابقون'
+  },
+  // Auth
+  auth: {
+    login: "تسجيل الدخول",
+    logout: "تسجيل الخروج",
+    email: 'البريد الإلكتروني او اسم المستخدم',
+    password: 'كلمة المرور'
+  },
+  // Speakers
+  speakers: 'المتحدثون',
+  // Footer
+  copyright: {
+    info: 'محتوى هذا الموقع مُرخّص برخصة المشاع الإبداعي Creative Commons نسب المصنف - الترخيص بالمثل 4.0 دولي (CC BY-SA 4.0),  إلا اذا ورد خلاف ذلك. '
+  },
+  careers: {
+    all: 'جميع الوظائف',
+    publishDate: 'تاريخ النشر',
+    type: 'نوع الوظيفة',
+    team: 'الفريق'
+  },
+  // Meta
+  meta: {
+    writtenBy: 'بقلم',
+    readTheReport:'اقرأ التقرير',
+    readMore: 'اقرأ المزيد',
+    knowMore: 'اعرف أكثر',
+    knowMoreAboutUs: 'اعرف أكثر عنا',
+    by: '',
+    download: 'تنزيل',
+    closed: 'مغلق',
+    visitWebsite: 'زيارة الموقع',
+    email: 'البريد الإلكتروني',
+    firstName: 'الاسم',
+    lastName: 'اسم العائلة',
+    password: 'كلمة المرور',
+    asReceived: 'كما ورد في رسالة التأكيد',
+    fullName: 'الاسم الكامل',
+    we: 'نحن',
+    and: 'و',
+    to: 'إلى',
+    translatedBy: 'ترجمة',
+  },
+  // Partners
+  partners: {
+    title: 'الشركاء والداعمون'
+  },
+  // Members
+  members: {
+    members: 'عضو'
+  },
+  // Sort
+  sort: {
+    sortBy: 'الترتيب حسب',
+    memberSinceACS: 'تاريخ التسجيل: تصاعدي',
+    memberSinceDESC: 'تاريخ التسجيل: تنازلي',
+    fullNameASC: 'الاسم: تصاعدي',
+    fullNameDESC: 'الاسم: تنازلي'
+  },
+  // Pagination
+  pagination: {
+    next: 'التالي',
+    prev: 'السابق'
+  },
+  // Impact
+  impact: {
+    title: "تأثير جوسا"
+  },
+  // Buttons
+  button: {
+    register: 'سجل',
+    join: 'انضم',
+    becomeAMember: 'اصبح عضوًا',
+    learnMoreWork: 'اعرف أكثر عن عملنا'
+  },
+  // Join Us
+  joinUs: {
+    title: 'انضم إلينا',
+    desc: 'مجتمع جوسا الرائع مكوّن من أعضاءنا "الجوساويين" المتميزين باهتماماتهم وخبراهم ومهارتهم والذين يبذلون جهدًا من أجل أردنّ منفتح تكنولوجيًا ومتمكن رقميًا.',
+  },
+  // Activities
+  activities: {
+    highlighted: 'الأنشطة'
+  },
+  // Dossier
+  dossier: {
+    dossier: 'ملف',
+    inDossier: 'في هذا الملف'
+  },
+  error: {
+    ops: 'عفوًا!',
+    lost: 'يبدو أننا لا نستطيع العثور على الصفحة التي تبحث عنها.',
+    links: 'إليك بعض الروابط المفيدة بدلاً من ذلك:',
+    home: 'الصفحة الرئيسية'
+  },
+  map: {
+    text: {
+      touch: "استخدم اصبعين لتحريك الخريطة",
+      scroll: "استخدم ctrl + scroll لتصغير/تكبير الخريطة",
+      scrollMac: "استخدم ⌘ + scroll لتصغير/تكبير الخريطة"
+    },
+  },
+  tooltip: {
+    copied: 'تم النسخ!'
+  },
+  timeString:{
+    time:'بتوقيت عمان'
+  },
+  addToCalendar:{
+    add:'أضف إلى التقويم',
+    google:'جوجل',
+    outlook:'أوتلوك',
+    office:'أوفيس 365',
+    apple:'آبل',
+    android:'أندرويد',
+    others:'تقويم آخر'
+  }
+}
