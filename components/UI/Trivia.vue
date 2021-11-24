5a143dd1b2faf3dc7d006ad4be7210586cdd9d85
thread-koder <mohamad.fberjawi@gmail.com>
2021-11-24 18:26:53 +0200

    Create the trivia component

---
+++ components/UI/Trivia.vue
@@ -0,0 +1,50 @@
+<template>
+  <div v-if="info['content_' + $i18n.locale]">
+    <div
+      class="px-6 md:px-12 preview"
+      :class="
+        alt ? 'text-center' : 'md:grid md:grid-cols-2 col-gap-8 row-gap-0'
+      "
+    >
+      <appImage
+        v-if="info.image"
+        :image="info.image"
+        :class="alt ? 'object-contain mx-auto mb-4' : 'thumbnail'"
+      />
+      <div :class="alt ? 'mx-auto w-full' : 'my-auto'">
+        <h3 class="mb-4">{{ info['title_' + $i18n.locale] }}</h3>
+        <p>{{ info['content_' + $i18n.locale] }}</p>
+      </div>
+    </div>
+    <div class="block py-8 my-4 mx-12 border-josa-warm-grey-dark">
+      <hr />
+    </div>
+  </div>
+</template>
+
+<script>
+import appImage from '~/components/UI/appImage'
+export default {
+  components: {
+    appImage,
+  },
+  props: {
+    info: {
+      type: Object,
+      required: true,
+    },
+    alt: {
+      type: Boolean,
+      default: false,
+    },
+  },
+}
+</script>
+
+<style scoped>
+.preview .thumbnail,
+.preview .thumbnail img {
+  width: 100%;
+  padding: 0;
+}
+</style>
