277571dbe561cf652597d75d71ab0b5907dcbc70
thread-koder <mohamad.fberjawi@gmail.com>
2021-11-24 16:35:46 +0200

    Add the global css files to the nuxt config file

--- nuxt.config.js
+++ nuxt.config.js
@@ -12,7 +12,12 @@ export default {
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [],
+  css: [
+    '@/assets/css/typography.css',
+    '@/assets/css/layout.css',
+    '@/assets/css/global.css',
+    '@/assets/css/fonts/ibm-plex.css',
+  ],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [],
