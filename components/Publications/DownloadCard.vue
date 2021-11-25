ee6465bf4037fa4f0f30f682daf813bebcd4e39e
thread-koder <mohamad.fberjawi@gmail.com>
2021-11-25 17:45:59 +0200

    Create the Download Card component

---
+++ components/Publications/DownloadCard.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="download-card">
+    <font-awesome-icon
+      class="icon ltr:mr-3 rtl:ml-3"
+      :icon="['fas', 'download']"
+    />
+    <div>
+      <h5 class="leading-none mb-1">{{ $t('meta.readTheReport') }}</h5>
+      <a :href="fileUrl" target="_blank">{{ $t('meta.download') }}</a>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'DownloadCard',
+  props: {
+    fileLink: {
+      type: Boolean,
+      required: true,
+    },
+  },
+  computed: {
+    fileUrl() {
+      return this.fileLink
+    },
+  },
+}
+</script>
+
+<style lang="postcss" scoped>
+.download-card {
+  @apply bg-josa-warm-grey-light p-8 flex align-top;
+}
+
+.icon {
+  @apply text-josa-warm-grey-dark text-2xl;
+}
+
+a {
+  @apply text-josa-blue;
+}
+</style>
