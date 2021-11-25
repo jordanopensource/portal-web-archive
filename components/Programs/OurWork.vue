a77a973598eca09697c9f6c21c203b776f3f974b
thread-koder <mohamad.fberjawi@gmail.com>
2021-11-25 17:32:03 +0200

    Create the our work component

---
+++ components/Programs/OurWork.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="our-work">
+    <h2 v-if="title" class="pb-2">{{ title }}</h2>
+    <programsList
+      class="grid grid-cols-1 md:grid-cols-2 gap-x-20"
+      :programs="programs"
+    />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'OurWork',
+  components: {
+    programsList: () => import('~/components/Programs/ProgramsList'),
+  },
+  props: {
+    title: {
+      type: String,
+      required: true,
+    },
+    programs: {
+      type: Array,
+      required: true,
+    },
+  },
+}
+</script>
