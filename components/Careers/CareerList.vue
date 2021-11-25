e3f699700d7da8f0df1c7fd31de47210fc5be227
thread-koder <mohamad.fberjawi@gmail.com>
2021-11-25 15:39:19 +0200

    Create the carrer components

---
+++ components/Careers/CareerList.vue
@@ -0,0 +1,153 @@
+<!-- Fetch and list careers based on career types-->
+<template>
+  <section v-if="ifNotEmpty()">
+    <h2 v-if="title">{{ title }}</h2>
+    <careerPreview
+      v-for="career in loadedCareers"
+      :id="'career' + career.id"
+      :key="career.id"
+      :career="career"
+    />
+    <!-- Pagination -->
+    <div
+      v-if="pageCount > 1"
+      class="pagination pt-6 text-center border-t border-dotted"
+    >
+      <span v-if="currentPage > 1" class="py-2"
+        ><a @click="fetchCurrentPage(currentPage - 1)">{{
+          $t('pagination.prev')
+        }}</a></span
+      >
+      <span class="py-2 inline-flex ltr:text-left rtl:text-right"
+        ><a
+          v-for="i in pageCount"
+          :key="i"
+          class="p-2"
+          :class="i == currentPage ? 'active' : ''"
+          @click="fetchCurrentPage(i)"
+          >{{ $t(i) }}</a
+        ></span
+      >
+      <span v-if="currentPage < pageCount" class="py-2"
+        ><a @click="fetchCurrentPage(currentPage + 1)">{{
+          $t('pagination.next')
+        }}</a></span
+      >
+    </div>
+  </section>
+</template>
+
+<script>
+import axios from 'axios'
+export default {
+  components: {
+    careerPreview: () => import('@/components/Careers/CareerPreview'),
+  },
+  props: {
+    title: {
+      type: String,
+      default: '',
+    },
+    careerType: {
+      type: String,
+      required: true,
+    },
+    limit: {
+      type: Number,
+      default: 10,
+    },
+  },
+  data() {
+    return {
+      sortBy: 'publishDate:DESC',
+      loadedCareers: [],
+      currentPage: 1,
+      start: 0,
+      count: 0,
+    }
+  },
+  computed: {
+    pageCount() {
+      return Math.ceil(this.count / this.limit)
+    },
+  },
+  created() {
+    this.fetchCareers()
+    this.countCareers()
+  },
+  methods: {
+    fetchCurrentPage(i) {
+      this.currentPage = i
+      this.start = this.limit * (this.currentPage - 1)
+      this.fetchCareers()
+    },
+    query() {
+      const args = []
+      let query = ''
+      if (this.start) {
+        const q = '_start=' + this.start
+        args.push(q)
+      }
+      if (this.limit) {
+        const q = '_limit=' + this.limit
+        args.push(q)
+      }
+      if (this.sortBy) {
+        const q = '_sort=' + this.sortBy
+        args.push(q)
+      }
+      if (this.careerType && this.careerType !== 'all') {
+        const q = 'careerType.name=' + this.careerType
+        args.push(q)
+      }
+      query = args.join('&')
+      return query
+    },
+    async fetchCareers() {
+      const query = this.query()
+      await axios
+        .get(process.env.baseUrl + '/careers?' + query)
+        .then((res) => {
+          const careersArray = []
+          for (const key in res.data) {
+            careersArray.push({
+              ...res.data[key],
+            })
+          }
+          this.loadedCareers = careersArray
+        })
+        .catch((e) => this.context.error(e))
+    },
+    async countCareers() {
+      const args = []
+      let query = ''
+      if (this.careerType && this.careerType !== 'all') {
+        const q = 'careerType.name=' + this.careerType
+        args.push(q)
+      }
+      query = args.join('&')
+      await axios.get(process.env.baseUrl + '/careers?' + query).then((res) => {
+        this.count = res.data.length
+      })
+    },
+    ifNotEmpty() {
+      if (Array.isArray(this.loadedCareers) && this.loadedCareers.length)
+        return true
+      else return false
+    },
+    limitNumberWithinRange(num, min, max) {
+      return Math.min(Math.max(parseInt(num), min), max)
+    },
+    calculateCurrentPage(num) {
+      this.currentPage = this.limitNumberWithinRange(num, 1, this.pageCount())
+      return this.currentPage
+    },
+  },
+}
+</script>
+
+<style lang="postcss" scoped>
+.pagination a.active {
+  @apply text-josa-blue-veryDark;
+}
+</style>
---
+++ components/Careers/CareerPreview.vue
@@ -0,0 +1,95 @@
+<template>
+  <div class="preview my-8 flex flex-wrap lg:flex-nowrap">
+    <nuxt-link tag="a" :to="careerLink" class="thumbnail">
+      <appImage
+        v-if="career.thumbnail"
+        :image="career.thumbnail"
+        size="medium"
+      />
+      <img v-else :src="placeholderImage" />
+    </nuxt-link>
+    <div ref="content" class="flex-grow">
+      <p v-if="career.status == false" class="closed">
+        {{ $t('meta.closed') }}
+      </p>
+      <nuxt-link :to="careerLink">
+        <h3>
+          {{
+            career['title_' + $i18n.locale]
+              ? career['title_' + $i18n.locale]
+              : career['title_en']
+          }}
+        </h3>
+      </nuxt-link>
+
+      <div class="career-info flex flex-wrap lg:flex-nowrap">
+        <span class="ltr:mr-4 rtl:ml-4">
+          <font-awesome-icon class="icon" :icon="['fas', 'clock']" />
+          {{ career['period_' + $i18n.locale] }}
+        </span>
+
+        <span>
+          <font-awesome-icon class="icon" :icon="['fas', 'map-marker-alt']" />
+          {{ career['location_' + $i18n.locale] }}
+        </span>
+      </div>
+
+      <nuxt-link :to="careerLink" class="py-4 display-more">
+        {{ $t('meta.knowMore') }}
+        <font-awesome-icon
+          class="ltr:ml-2 rtl:mr-2 align-middle"
+          :icon="['fas', arrowIcon]"
+        />
+      </nuxt-link>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CareerPreview',
+  components: {
+    appImage: () => import('~/components/UI/appImage'),
+  },
+  props: {
+    career: {
+      type: Object,
+      required: true,
+    },
+  },
+  data() {
+    return {
+      placeholderImage: process.env.bucketUrl + '/josabots_88f0a93786.jpeg',
+    }
+  },
+  computed: {
+    careerLink() {
+      let title = ''
+      if (this.career['title_' + this.$i18n.locale]) {
+        title = this.career['title_' + this.$i18n.locale]
+      } else {
+        title = this.career.title_en
+      }
+      const slug = this.$options.filters.stringToSlug(title)
+      return this.localePath('/careers/' + this.career.id + '/' + slug)
+    },
+    arrowIcon() {
+      if (this.$i18n.locale === 'ar') {
+        return 'long-arrow-alt-left'
+      } else {
+        return 'long-arrow-alt-right'
+      }
+    },
+  },
+}
+</script>
+
+<style lang="postcss" scoped>
+p {
+  @apply leading-golden;
+}
+
+.icon {
+  @apply text-josa-warm-grey-dark;
+}
+</style>
