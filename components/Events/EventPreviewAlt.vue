2dcbcd849c59575247da7deba84e311bb6b9c72f
Mohamad Berjawi <mohamad.fberjawi@gmail.com>
2021-11-29 22:01:58 +0200

    Import the event preview components

---
+++ components/Events/EventPreviewAlt.vue
@@ -0,0 +1,48 @@
+<template>
+  <div>
+    <div class="mb-2">
+      <h5 class="py-2 text-josa-black inline">
+        {{ event.category['title_' + $i18n.locale] }}
+      </h5>
+      <span class="leading-none"> | </span>
+      <h5 class="py-2 text-josa-blue inline">
+        {{ event.startDate | fullDate($i18n.locale) }}
+      </h5>
+    </div>
+    <nuxt-link :to="eventLink">
+      <h3>
+        {{
+          event['title_' + $i18n.locale]
+            ? event['title_' + $i18n.locale]
+            : event['title_en']
+        }}
+      </h3>
+    </nuxt-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'EventPreviewAlt',
+  props: {
+    event: {
+      type: Object,
+      required: true,
+    },
+  },
+  computed: {
+    eventLink() {
+      let title = ''
+      if (this.event['title_' + this.$i18n.locale]) {
+        title = this.event['title_' + this.$i18n.locale]
+      } else {
+        title = this.event.title_en
+      }
+      const slug = this.$options.filters.stringToSlug(title)
+      return this.localePath('/events/' + this.event.id + '/' + slug)
+    },
+  },
+}
+</script>
+
+<style scoped></style>
