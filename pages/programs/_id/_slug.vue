89b5d5e4d16a554903779b09cc90460dcffc1a82
Mohamad Berjawi <mohamad.fberjawi@gmail.com>
2021-11-29 22:53:53 +0200

    Import the programs page

---
+++ pages/programs/_id/_slug.vue
@@ -0,0 +1,54 @@
+<template>
+  <programSingle :id="'program-' + program.id" :program="program" />
+</template>
+
+<script>
+import axios from 'axios'
+import programSingle from '~/components/Programs/ProgramSingle'
+
+export default {
+  components: {
+    programSingle,
+  },
+  layout: 'default',
+  asyncData(context) {
+    return axios
+      .get(process.env.baseUrl + '/programs/' + context.params.id)
+      .then((res) => {
+        return {
+          program: res.data,
+        }
+      })
+      .catch((e) => context.error(e))
+  },
+  head() {
+    const i18nSeo = this.$nuxtI18nSeo()
+    return {
+      title:
+        this.pageTitle +
+        ' - ' +
+        (this.$i18n.locale === 'ar'
+          ? 'الجمعية الأردنية للمصدر المفتوح'
+          : 'Jordan Open Source Association'),
+      meta: [
+        {
+          name: 'description',
+          content: this.program['metaDescription_' + this.$i18n.locale]
+            ? this.program['metaDescription_' + this.$i18n.locale]
+            : this.program['description_' + this.$i18n.locale] || '',
+        },
+        ...i18nSeo.meta,
+      ],
+    }
+  },
+  computed: {
+    pageTitle() {
+      try {
+        return this.program['title_' + this.$i18n.locale]
+      } catch {
+        return this.program.title_en
+      }
+    },
+  },
+}
+</script>
---
+++ pages/programs/index.vue
@@ -0,0 +1,65 @@
+<template>
+  <div class="programs-page">
+    <!-- Banner -->
+    <pageBanner :page-meta="programsMeta" />
+
+    <!-- Lists -->
+    <div class="container pb-20">
+      <programsList class="programs-list" />
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+import pageBanner from '~/components/UI/PageBanner'
+import programsList from '~/components/Programs/ProgramsList'
+
+export default {
+  components: {
+    pageBanner,
+    programsList,
+  },
+  layout: 'default',
+  async asyncData(context) {
+    const pageMeta = await axios.get(
+      process.env.baseUrl + '/page-metas?pageId=programs'
+    )
+    return {
+      programsMeta: pageMeta.data[0],
+    }
+  },
+  head() {
+    const i18nSeo = this.$nuxtI18nSeo()
+    return {
+      title:
+        this.programsMeta['title_' + this.$i18n.locale] +
+        ' - ' +
+        (this.$i18n.locale === 'ar'
+          ? 'الجمعية الأردنية للمصدر المفتوح'
+          : 'Jordan Open Source Association'),
+      meta: [
+        {
+          name: 'description',
+          content: this.programsMeta['metaDescription_' + this.$i18n.locale]
+            ? this.programsMeta['metaDescription_' + this.$i18n.locale]
+            : '',
+        },
+        ...i18nSeo.meta,
+      ],
+    }
+  },
+}
+</script>
+
+<style lang="postcss" scoped>
+.programs-list {
+  @apply px-6 mt-20;
+}
+
+@screen md {
+  .programs-list {
+    @apply px-12;
+  }
+}
+</style>
