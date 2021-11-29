a69b47384ead9e6eb98f046537e94b2b50d40c0e
Mohamad Berjawi <mohamad.fberjawi@gmail.com>
2021-11-29 19:14:37 +0200

    Import the events page

---
+++ pages/events/_id/_slug.vue
@@ -0,0 +1,55 @@
+<template>
+  <EventSingle :event="event" />
+</template>
+
+<script>
+import axios from 'axios'
+import EventSingle from '~/components/Events/EventSingle'
+export default {
+  components: {
+    EventSingle,
+  },
+  layout: 'default',
+  asyncData(context) {
+    return axios
+      .get(process.env.baseUrl + '/events/' + context.params.id)
+      .then((res) => {
+        return {
+          event: res.data,
+        }
+      })
+      .catch((e) => context.error(e))
+  },
+  head() {
+    const i18nSeo = this.$nuxtI18nSeo()
+    return {
+      title:
+        this.pageTitle +
+        ' - ' +
+        (this.$i18n.locale === 'ar'
+          ? 'الجمعية الأردنية للمصدر المفتوح'
+          : 'Jordan Open Source Association'),
+      meta: [
+        {
+          name: 'description',
+          content: this.event['metaDescription_' + this.$i18n.locale]
+            ? this.event['metaDescription_' + this.$i18n.locale]
+            : '',
+        },
+        ...this.$options.filters.ogTags(
+          'event',
+          this.event,
+          this.$route.path,
+          this.$i18n.locale
+        ),
+        ...i18nSeo.meta,
+      ],
+    }
+  },
+  computed: {
+    pageTitle() {
+      return this.event['title_' + this.$i18n.locale]
+    },
+  },
+}
+</script>
---
+++ pages/events/index.vue
@@ -0,0 +1,110 @@
+<template>
+  <div class="event-page">
+    <!-- Banner -->
+    <pageBanner :page-meta="eventMeta" />
+    <!-- Menu -->
+    <div class="bg-josa-black py-8">
+      <div class="container">
+        <div class="px-6 md:px-12 flex flex-col sm:flex-row">
+          <span
+            :class="activeCat == 'all' ? 'active' : ''"
+            class="cat-link"
+            @click="setActiveCat('all')"
+            >{{ $t('eventCats.all') }}</span
+          >
+          <span
+            v-for="cat in eventCategories"
+            :key="cat.id"
+            :class="activeCat == cat.name ? 'active' : ''"
+            class="cat-link"
+            @click="setActiveCat(cat.name)"
+            >{{ cat['title_' + $i18n.locale] }}</span
+          >
+        </div>
+      </div>
+    </div>
+    <!-- Upcoming Events -->
+    <div class="container pb-8 mt-20">
+      <div class="px-6 md:px-12">
+        <h2 class="mb-8">{{ $t('events.upcoming') }}</h2>
+        <eventListUpcoming v-if="activeCat == 'all'" category="all" />
+        <template v-for="cat in eventCategories">
+          <eventListUpcoming
+            v-if="activeCat == cat.name"
+            :key="cat.id"
+            :category="cat.name"
+          />
+        </template>
+      </div>
+    </div>
+    <!-- Previous Events -->
+    <div class="container pb-8 mt-20">
+      <div class="px-6 md:px-12">
+        <h2 class="mb-8">{{ $t('events.previous') }}</h2>
+        <eventListPrevious v-if="activeCat == 'all'" category="all" />
+        <template v-for="cat in eventCategories">
+          <eventListPrevious
+            v-if="activeCat == cat.name"
+            :key="cat.id"
+            :category="cat.name"
+          />
+        </template>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+import eventListUpcoming from '@/components/Events/EventList'
+import eventListPrevious from '@/components/Events/EventListPrevious'
+import pageBanner from '@/components/UI/PageBanner'
+export default {
+  components: {
+    eventListUpcoming,
+    eventListPrevious,
+    pageBanner,
+  },
+  layout: 'default',
+  async asyncData(context) {
+    const pageMeta = await axios.get(
+      process.env.baseUrl + '/page-metas?pageId=event'
+    )
+    const cats = await axios.get(process.env.baseUrl + '/event-categories')
+    return {
+      eventMeta: pageMeta.data[0],
+      eventCategories: cats.data,
+    }
+  },
+  data() {
+    return {
+      activeCat: 'all',
+    }
+  },
+  head() {
+    const i18nSeo = this.$nuxtI18nSeo()
+    return {
+      title:
+        this.eventMeta['title_' + this.$i18n.locale] +
+        ' - ' +
+        (this.$i18n.locale === 'ar'
+          ? 'الجمعية الأردنية للمصدر المفتوح'
+          : 'Jordan Open Source Association'),
+      meta: [
+        {
+          name: 'description',
+          content: this.eventMeta['metaDescription_' + this.$i18n.locale]
+            ? this.eventMeta['metaDescription_' + this.$i18n.locale]
+            : '',
+        },
+        ...i18nSeo.meta,
+      ],
+    }
+  },
+  methods: {
+    setActiveCat(cat) {
+      this.activeCat = cat
+    },
+  },
+}
+</script>
