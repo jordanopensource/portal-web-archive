0fd84cb002d4bd04a0c4711a0ec67343a1da864f
Mohamad Berjawi <mohamad.fberjawi@gmail.com>
2021-11-29 22:41:59 +0200

    Import the partners page

---
+++ pages/partners-and-supporters/index.vue
@@ -0,0 +1,65 @@
+<template>
+  <div class="partners-page">
+    <!-- Banner -->
+    <pageBanner :page-meta="partnersMeta" />
+
+    <!-- Lists -->
+    <div class="container pb-20">
+      <partnersList class="partners-list" :title="$t('partners.title')" />
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+import pageBanner from '@/components/UI/PageBanner'
+import partnersList from '@/components/Partners/PartnersList'
+
+export default {
+  components: {
+    pageBanner,
+    partnersList,
+  },
+  layout: 'default',
+  async asyncData(context) {
+    const pageMeta = await axios.get(
+      process.env.baseUrl + '/page-metas?pageId=partners'
+    )
+    return {
+      partnersMeta: pageMeta.data[0],
+    }
+  },
+  head() {
+    const i18nSeo = this.$nuxtI18nSeo()
+    return {
+      title:
+        this.partnersMeta['title_' + this.$i18n.locale] +
+        ' - ' +
+        (this.$i18n.locale === 'ar'
+          ? 'الجمعية الأردنية للمصدر المفتوح'
+          : 'Jordan Open Source Association'),
+      meta: [
+        {
+          name: 'description',
+          content: this.partnersMeta['metaDescription_' + this.$i18n.locale]
+            ? this.partnersMeta['metaDescription_' + this.$i18n.locale]
+            : '',
+        },
+        ...i18nSeo.meta,
+      ],
+    }
+  },
+}
+</script>
+
+<style lang="postcss" scoped>
+.partners-list {
+  @apply px-6 mt-20;
+}
+
+@screen md {
+  .partners-list {
+    @apply px-12;
+  }
+}
+</style>
