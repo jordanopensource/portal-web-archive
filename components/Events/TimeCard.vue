aba0ba38304de72d1305e5dee8528e44f3f62f50
Mohamad Berjawi <mohamad.fberjawi@gmail.com>
2021-11-29 22:16:13 +0200

    Import the time card component

---
+++ components/Events/TimeCard.vue
@@ -0,0 +1,68 @@
+<template>
+  <div class="time-card">
+    <font-awesome-icon
+      class="icon ltr:mr-3 rtl:ml-3"
+      :icon="['fas', 'clock']"
+    />
+    <div>
+      <h5>{{ $t('timeCard.title') }}</h5>
+      <p>{{ from | dayFullDateAmman($i18n.locale) }}</p>
+      <p>
+        {{ $t('timeCard.from') }} {{ from | timeAmman($i18n.locale) }}
+        {{ $t('timeCard.to') }} {{ to | timeAmman($i18n.locale) }} ({{
+          $t('timeString.time')
+        }})
+      </p>
+      <br />
+      <div v-if="new Date().getTimezoneOffset() !== -180">
+        <p>{{ from | dayFullDate($i18n.locale) }}</p>
+        <p>
+          {{ $t('timeCard.from') }} {{ from | time($i18n.locale) }}
+          {{ $t('timeCard.to') }} {{ to | time($i18n.locale) }} ({{
+            $t('timeCard.gmt')
+          }}{{ to | timezone($i18n.locale) }})
+        </p>
+      </div>
+      <addToCalendar v-if="isOngoingEvent()" class="mt-4" :event="event" />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TimeCard',
+  components: {
+    addToCalendar: () => import('~/components/Events/AddToCalendar'),
+  },
+  props: {
+    from: {
+      type: String,
+      required: true,
+    },
+    to: {
+      type: String,
+      required: true,
+    },
+    event: {
+      type: Object,
+      required: true,
+    },
+  },
+  methods: {
+    isOngoingEvent() {
+      const endDate = new Date(this.to).getTime()
+      const currentDate = new Date().getTime()
+      return currentDate < endDate
+    },
+  },
+}
+</script>
+
+<style lang="postcss" scoped>
+.time-card {
+  @apply bg-josa-warm-grey-light p-8 flex align-top;
+}
+.icon {
+  @apply text-josa-warm-grey-dark text-2xl;
+}
+</style>
