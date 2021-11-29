6cb35e0bfb112d5fd2e590d5a1e86a296e86006c
Mohamad Berjawi <mohamad.fberjawi@gmail.com>
2021-11-29 22:07:39 +0200

    Import the online event card component

---
+++ components/Events/OnlineEventCard.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <div class="online-card">
+      <font-awesome-icon
+        class="icon ltr:mr-3 rtl:ml-3"
+        :icon="['fas', 'globe']"
+      />
+      <h5>{{ $t('events.onlineEvent') }}</h5>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'OnlineEvent',
+}
+</script>
+
+<style lang="postcss" scoped>
+.online-card {
+  @apply bg-josa-warm-grey-light p-8 flex flex-row align-top;
+}
+
+.icon {
+  @apply text-josa-warm-grey-dark text-2xl;
+}
+</style>
