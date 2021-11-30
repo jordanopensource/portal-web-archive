9a94a0000a29a48a064eee726e4b773914c6b713
Mohamad Berjawi <mohamad.fberjawi@gmail.com>
2021-11-30 13:13:45 +0200

    Refator the tailwind css classes

--- components/UI/Trivia.vue
+++ components/UI/Trivia.vue
@@ -2,9 +2,7 @@
   <div v-if="info['content_' + $i18n.locale]">
     <div
       class="px-6 md:px-12 preview"
-      :class="
-        alt ? 'text-center' : 'md:grid md:grid-cols-2 col-gap-8 row-gap-0'
-      "
+      :class="alt ? 'text-center' : 'md:grid md:grid-cols-2 gap-x-8 gap-y-0'"
     >
       <appImage
         v-if="info.image"
