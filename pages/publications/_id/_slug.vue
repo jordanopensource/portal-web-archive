4c76e745ec1cb0a5d36d45429cb8f1dd228abe6e
Mohamad Berjawi <mohamad.fberjawi@gmail.com>
2021-11-30 12:25:27 +0200

    Import the publication page

---
+++ pages/publications/_id/_slug.vue
@@ -0,0 +1,55 @@
+<template>
+  <publicationSingle :publication="publication" />
+</template>
+
+<script>
+import axios from 'axios'
+import publicationSingle from '~/components/Publications/PublicationSingle'
+export default {
+  components: {
+    publicationSingle,
+  },
+  layout: 'default',
+  asyncData(context) {
+    return axios
+      .get(process.env.baseUrl + '/publications/' + context.params.id)
+      .then((res) => {
+        return {
+          publication: res.data,
+        }
+      })
+      .catch((e) => context.error(e))
+  },
+  head() {
+    const i18nSeo = this.$nuxtI18nSeo()
+    return {
+      title:
+        this.pageTitle +
+        ' - ' +
+        (this.$i18n.locale === 'ar'
+          ? 'الجمعية الأردنية للمصدر المفتوح'
+          : 'Jordan Open Source Association'),
+      meta: [
+        {
+          name: 'description',
+          content: this.publication['excerpt_' + this.$i18n.locale]
+            ? this.publication['excerpt' + this.$i18n.locale]
+            : '',
+        },
+        ...this.$options.filters.ogTags(
+          'publication',
+          this.publication,
+          this.$route.path,
+          this.$i18n.locale
+        ),
+        ...i18nSeo.meta,
+      ],
+    }
+  },
+  computed: {
+    pageTitle() {
+      return this.publication['title_' + this.$i18n.locale]
+    },
+  },
+}
+</script>
---
+++ pages/publications/index.vue
@@ -0,0 +1,106 @@
+<template>
+  <div class="publication-page">
+    <!-- Banner -->
+    <pageBanner :page-meta="publicationMeta" />
+    <!-- Menu -->
+    <div class="bg-josa-black py-8">
+      <div class="container">
+        <div class="px-6 md:px-12 flex flex-col sm:flex-row">
+          <span
+            :class="activeCat == 'all' ? 'active' : ''"
+            class="cat-link"
+            @click="setActiveCat('all')"
+            >{{ $t('publicationCats.title.all') }}</span
+          >
+          <span
+            v-for="cat in publicationCategories"
+            :key="cat.id"
+            :class="activeCat == cat.name ? 'active' : ''"
+            class="cat-link"
+            @click="setActiveCat(cat.name)"
+            >{{ cat['title_' + $i18n.locale] }}</span
+          >
+        </div>
+      </div>
+    </div>
+    <!-- List -->
+    <div class="container pb-20">
+      <div v-for="cat in publicationCategories" :key="cat.id">
+        <publicationList
+          v-if="activeCat == 'all' || activeCat == cat.name"
+          class="publication-list"
+          :category="cat.name"
+          :title="cat['title_' + $i18n.locale]"
+        />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+import publicationList from '@/components/Publications/PublicationList'
+import pageBanner from '@/components/UI/PageBanner'
+
+export default {
+  components: {
+    publicationList,
+    pageBanner,
+  },
+  layout: 'default',
+  async asyncData(context) {
+    const pageMeta = await axios.get(
+      process.env.baseUrl + '/page-metas?pageId=publications'
+    )
+    const cats = await axios.get(
+      process.env.baseUrl + '/publication-categories'
+    )
+    return {
+      publicationMeta: pageMeta.data[0],
+      publicationCategories: cats.data,
+    }
+  },
+  data() {
+    return {
+      activeCat: 'all',
+    }
+  },
+  head() {
+    const i18nSeo = this.$nuxtI18nSeo()
+    return {
+      title:
+        this.publicationMeta['title_' + this.$i18n.locale] +
+        ' - ' +
+        (this.$i18n.locale === 'ar'
+          ? 'الجمعية الأردنية للمصدر المفتوح'
+          : 'Jordan Open Source Association'),
+      meta: [
+        {
+          name: 'description',
+          content: this.publicationMeta['metaDescription_' + this.$i18n.locale]
+            ? this.publicationMeta['metaDescription_' + this.$i18n.locale]
+            : '',
+        },
+        ...i18nSeo.meta,
+      ],
+    }
+  },
+  methods: {
+    setActiveCat(cat) {
+      this.activeCat = cat
+    },
+  },
+}
+</script>
+
+<style lang="postcss" scoped>
+.publication-list {
+  @apply px-6 mt-20;
+}
+
+@screen md {
+  .publication-list {
+    @apply px-12;
+  }
+}
+</style>
