3c3e87f87b1a6efd45deb0407287946ca299bb99
thread-koder <mohamad.fberjawi@gmail.com>
2022-02-10 17:13:30 +0200

    Refactor the og-tags js file

--- plugins/og-tags.js
+++ plugins/og-tags.js
@@ -1,10 +1,10 @@
-import Vue from 'vue';
+import Vue from 'vue'
 
 function ogData(type, content, path, lang) {
-  var title = ''
-  var category = ''
-  var imageUrl = ''
-  var description = ''
+  let title = ''
+  let category = ''
+  let imageUrl = ''
+  let description = ''
   // Set image url
   if (content.ogData && content.ogData.image) {
     imageUrl = content.ogData.image.url
@@ -14,13 +14,13 @@ function ogData(type, content, path, lang) {
     imageUrl = ''
   }
   // Set title
-  if (type == 'article') {
+  if (type === 'article') {
     title = content.title
   } else {
     title = content['title_' + lang]
   }
   // Set category
-  if (type == 'career') {
+  if (type === 'career') {
     category = content.careerType['title_' + lang]
   } else if (content.category) {
     category = content.category['title_' + lang]
@@ -40,92 +40,93 @@ function ogData(type, content, path, lang) {
     description = title
   }
   // Set data
-  let data = {
-    title: title,
+  const data = {
+    title,
     url: 'https://josa.ngo' + path,
-    type: type,
-    publishedTime: content.publishDate ? content.publishDate : content.created_at,
+    type,
+    publishedTime: content.publishDate
+      ? content.publishDate
+      : content.created_at,
     modifiedTime: content.created_at,
     updatedTime: content.updated_at,
-    category: category,
+    category,
     image: imageUrl,
-    description: description
+    description,
   }
   return data
 }
 
-
 const ogTags = (type, content, path, lang) => {
-  let data = ogData(type, content, path, lang);
-  let tags = [
+  const data = ogData(type, content, path, lang)
+  const tags = [
     // Facebook Open Graph
     {
       property: 'og:site_name',
-      content: 'Jordan Open Source Association'
+      content: 'Jordan Open Source Association',
     },
     {
       property: 'og:title',
-      content: data.title
+      content: data.title,
     },
     {
       property: 'og:url',
-      content: data.url
+      content: data.url,
     },
     {
       property: 'og:type',
-      content: data.type
+      content: data.type,
     },
     {
       property: 'og:description',
-      content: data.description
+      content: data.description,
     },
     {
       property: 'og:updated_time',
-      content: data.updatedTime
+      content: data.updatedTime,
     },
     {
       property: 'article:published_time',
-      content: data.publishedTime
+      content: data.publishedTime,
     },
     {
       property: 'article:modified_time',
-      content: data.modifiedTime
+      content: data.modifiedTime,
     },
     {
       property: 'article:section',
-      content: data.category
+      content: data.category,
     },
     {
       property: 'article:publisher',
-      content: 'https://facebook.com/jordanopensource'
+      content: 'https://facebook.com/jordanopensource',
     },
     {
       property: 'og:image',
-      content: data.image
+      content: data.image,
     },
     // Twitter Cards
     {
       name: 'twitter:title',
-      content: data.title
+      content: data.title,
     },
     {
       name: 'twitter:url',
-      content: data.url
+      content: data.url,
     },
     {
       name: 'twitter:description',
-      content: data.description
+      content: data.description,
     },
     {
       name: 'twitter:card',
-      content: 'summary_large_image'
+      content: 'summary_large_image',
     },
     {
       name: 'twitter:image',
-      content: data.image
-    }
+      content: data.image,
+    },
   ]
   return tags
-};
+}
 
 Vue.filter('ogTags', ogTags)
