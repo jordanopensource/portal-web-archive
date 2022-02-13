837d3e6128a60939ce1f8275f10470aa67b015b7
George Khoury <geokhoury@users.noreply.github.com>
2022-02-13 16:37:46 +0200

    set Matomo siteID to development site

--- static/js/matomo.js
+++ static/js/matomo.js
@@ -7,7 +7,7 @@ _paq.push(['enableLinkTracking'])
 ;(function () {
   let u = '//analytics.jordanopensource.org/'
   _paq.push(['setTrackerUrl', u + 'matomo.php'])
-  _paq.push(['setSiteId', '2'])
+  _paq.push(['setSiteId', '7'])
   let d = document
   let g = d.createElement('script')
   let s = d.getElementsByTagName('script')[0]
