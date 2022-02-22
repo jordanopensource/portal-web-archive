80f86e2c3d2281606e7e03ce7d6fe38443317665
thread-koder <mohamad.fberjawi@gmail.com>
2022-02-22 15:06:58 +0200

    Create the createOffice365CalendarLink function

--- components/Events/AddToCalendar.vue
+++ components/Events/AddToCalendar.vue
@@ -37,9 +37,12 @@
         </div>
         <div class="flex">
           <img class="icon" src="~/static/images/icons/office.webp" />
-          <a class="list-text" @click="outlookEncode(officeLink)">{{
-            $t('addToCalendar.office')
-          }}</a>
+          <a
+            class="list-text"
+            target="_blank"
+            :href="createOffice365CalendarLink()"
+            >{{ $t('addToCalendar.office') }}</a
+          >
         </div>
         <div class="flex">
           <img class="icon" src="~/static/images/icons/android.webp" />
@@ -66,7 +69,7 @@
   </div>
 </template>
 <script>
-import { google, outlook } from 'calendar-link'
+import { google, outlook, office365 } from 'calendar-link'
 export default {
   name: 'AddToCalender',
   components: {
@@ -170,6 +173,41 @@ export default {
       const outlookCalendarLink = outlook(event)
 
       return outlookCalendarLink
+    },
+    createOffice365CalendarLink() {
+      /**
+       * creates an office365 calendar link based on the event object
+       */
+      const eventStartDate = this.event.startDate
+      const eventEndDate = this.event.endDate
+      const diffMs = eventEndDate - eventStartDate // return the difference in milliseconds between the dates
+      const eventDuration = Math.round(((diffMs % 86400000) % 3600000) / 60000) // return the event duration in minutes
+
+      /**
+       * create the event object
+       * @type {Object}
+       * @requires @property {String} title - the event title
+       * @requires @property {Date} start - the event Start time
+       * @property {String} description - the event description
+       * @property {Date} end - the event end time
+       * @property {Array} duration - the event duration with value (Number) and unit (String)
+       * @property {String} location - the event location
+       */
+      const event = {
+        title: this.event['title_' + this.$i18n.locale],
+        description: this.event['description_' + this.$i18n.locale]
+          ? this.event['description_' + this.$i18n.locale]
+          : '',
+        start: this.event.startDate,
+        end: this.event.endDate,
+        duration: [eventDuration, 'minutes'],
+        location: this.event.address
+          ? this.event.address['addressOne_' + this.$i18n.locale]
+          : '',
+      }
+      const office365CalendarLink = office365(event)
+
+      return office365CalendarLink
     },
     convertDate(date) {
       let event = new Date(date).toISOString()
