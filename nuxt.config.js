7c93664face59e0f82bbb6b51312fe15293ede11
thread-koder <mohamad.fberjawi@gmail.com>
2022-03-21 17:56:08 +0300

    Update the nuxt config file

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,4 +1,6 @@
-export default {
+import { defineNuxtConfig } from '@nuxt/bridge'
+
+export default defineNuxtConfig({
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     title: 'portal-web-beta',
@@ -184,5 +186,6 @@ export default {
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
     extractCSS: true,
+    transpile: ['tailwind-config-viewer'],
   },
-}
+})
