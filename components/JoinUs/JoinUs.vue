ee3dba9b88649514080a338226b3f1a7e4eb5830
Issa Mahasneh <issa86@gmail.com>
2022-03-30 15:51:41 +0300

    Change the Become a Member link to online form

--- components/JoinUs/JoinUs.vue
+++ components/JoinUs/JoinUs.vue
@@ -6,7 +6,7 @@
           <h2>{{ $t('joinUs.title') }}</h2>
           <p class="my-8">{{ $t('joinUs.desc') }}</p>
           <a
-            href="https://members.jordanopensource.org"
+            href="https://link.josa.ngo/join-josa"
             target="_blank"
             class="button-sharp button-dark-blue-full mb-8 inline-block"
             >{{ $t('button.becomeAMember') }}</a
