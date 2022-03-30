42979f087dac5c8e682e350f45f550f63195c4e8
linuxjo <marina.alsahawneh@gmail.com>
2022-03-30 16:58:52 +0300

    Fix indentation

--- components/Events/EventsSpotlight.vue
+++ components/Events/EventsSpotlight.vue
@@ -1,31 +1,31 @@
 <!-- Show events only if there are upcoming events -->
 <template>
   <section v-if="ifNotEmpty()">
-      <div class="container">
-        <div class="px-6 md:px-12 mt-20">
-          <div class="flex flex-wrap md:flex-no-wrap justify-between items-baseline">
-            <h2 class="mb-8">{{ $t('events.upcoming') }}</h2>
-            <nuxt-link :to="localePath('events')">
-              <h6 class="display-more">
-                {{ $t('eventCats.all') }}
-                <font-awesome-icon
-                  class="ltr:ml-2 rtl:mr-2 align-middle"
-                  :icon="['fas', arrowIcon ]"
-                />
-              </h6>
-            </nuxt-link>
-          </div>
+    <div class="container">
+      <div class="px-6 md:px-12 mt-20">
+        <div class="flex flex-wrap md:flex-no-wrap justify-between items-baseline">
+          <h2 class="mb-8">{{ $t('events.upcoming') }}</h2>
+          <nuxt-link :to="localePath('events')">
+            <h6 class="display-more">
+              {{ $t('eventCats.all') }}
+              <font-awesome-icon
+                class="ltr:ml-2 rtl:mr-2 align-middle"
+                :icon="['fas', arrowIcon ]"
+              />
+            </h6>
+          </nuxt-link>
         </div>
-        <div v-for="month in months" :key="month" class="mb-20">
-          <eventPreview
-            class="px-6 md:px-12"
-            v-for="event in sortedEvents[month]"
-            :id="event.id"
-            :key="event.id"
-            :event="event"
-          />
-        </div>
-       </div>
+      </div>
+      <div v-for="month in months" :key="month" class="mb-20">
+        <eventPreview
+          class="px-6 md:px-12"
+          v-for="event in sortedEvents[month]"
+          :id="event.id"
+          :key="event.id"
+          :event="event"
+        />
+      </div>
+    </div>
   </section>
 </template>
 
