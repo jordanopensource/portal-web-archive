398a4685c8ec5c004622142e62017dc9a252394c
thread-koder <mohamad.fberjawi@gmail.com>
2022-06-09 15:44:05 +0300

    Use the OTS API endpoints for the feedback form

--- components/Contact/FeedbackForm.vue
+++ components/Contact/FeedbackForm.vue
@@ -1,5 +1,5 @@
 <template>
-  <form @submit.prevent="onSubmit">
+  <form class="bg-josa-warm-grey p-8" @submit.prevent="onSubmit">
     <AppControlInput v-model="form.subject" required>{{
       $t('feedback.form.subject')
     }}</AppControlInput>
@@ -17,11 +17,13 @@
         $t('feedback.form.submit')
       }}</AppButton>
     </div>
+    <client-only>
+      <notifications classes="my-style" group="feedback" />
+    </client-only>
   </form>
 </template>
 
 <script>
-import axios from 'axios'
 export default {
   name: 'FeedbackForm',
   components: {
@@ -41,34 +43,83 @@ export default {
   },
   methods: {
     async onSubmit() {
-      await axios
-        .post(process.env.baseUrl + '/feedbacks', this.form)
+      await this.$axios
+        .post(
+          '/ots/tickets',
+          {
+            title: `${this.form.subject}`,
+            group: `${this.$config.otsFormGroup}`,
+            customer_id: `guess:${this.form.email}`,
+            article: {
+              from: `${this.form.email}`,
+              subject: `${this.form.subject}`,
+              internal: false,
+              sender: 'Customer',
+              type: 'web',
+              body: `${this.form.description}`,
+            },
+          },
+          {
+            headers: {
+              Authorization: `Bearer ${this.$config.otsToken}`,
+            },
+          }
+        )
         .then((res) => {
-          if (res.status === 200) {
-            this.success()
+          if (res.status === 201) {
+            this.$notify({
+              group: 'feedback',
+              type: 'success',
+              title: 'Your inquiry has been sent',
+              text: 'We will get back to you as soon as possible.',
+              width: '35%',
+            })
           } else {
-            this.fail()
+            this.$notify({
+              group: 'feedback',
+              type: 'error',
+              text: 'There was an error while sending your inquiry, please try again later.',
+              width: '35%',
+            })
           }
         })
-    },
-    success() {
-      this.flashMessage.setStrategy('single')
-      this.flashMessage.success({
-        message: this.$t('feedback.sent'),
-      })
-    },
-    fail() {
-      this.flashMessage.setStrategy('single')
-      this.flashMessage.error({
-        message: this.$t('feedback.fail'),
-      })
     },
   },
 }
 </script>
 
-<style lang="postcss" scoped>
-form {
-  @apply bg-josa-warm-grey p-8;
+<style lang="scss">
+.my-style {
+  padding: 10px;
+  margin: 0 5px 5px;
+
+  font-size: 12px;
+
+  color: #ffffff;
+  background: #44a4fc;
+  border-left: 5px solid #187fe7;
+
+  .notification-title {
+    font-size: 1.1rem;
+  }
+
+  .notification-content {
+    font-size: 1rem;
+  }
+
+  &.warn {
+    background: #ffb648;
+    border-left-color: #f48a06;
+  }
+
+  &.error {
+    background: #e54d42;
+    border-left-color: #b82e24;
+  }
+
+  &.success {
+    background: #68cd86;
+    border-left-color: #42a85f;
+  }
 }
 </style>
