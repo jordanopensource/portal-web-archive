4967ffa7614f968f330860798f93a4d610785225
itsmohmans <mohmans@pm.me>
2022-08-22 18:52:48 +0300

    fix indentations

--- components/Author/ArticlesList.vue
+++ components/Author/ArticlesList.vue
@@ -1,26 +1,26 @@
 <template>
   <div>
-      <template v-for="article in articles">
-        <articlePreview
-          :id="article.id"
-          :key="article.id"
-          :article="article"
-        />
-      </template>
-    </div>
+    <template v-for="article in articles">
+      <articlePreview
+        :id="article.id"
+        :key="article.id"
+        :article="article"
+      />
+    </template>
+  </div>
 </template>
 
 <script>
 export default {
-    components: {
-        articlePreview: () => import("@/components/Blog/ArticlePreview.vue"),
-    },
-    props: {
-        articles: {
-            type: Array,
-            required: true
-        }
-    },
+  components: {
+    articlePreview: () => import("@/components/Blog/ArticlePreview.vue"),
+  },
+  props: {
+    articles: {
+      type: Array,
+      required: true
+    }
+  },
 }
 </script>
 
