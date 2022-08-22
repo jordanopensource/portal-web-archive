2aa3c0270a1e6b3f4053a4b1e238b2a55a4b2857
itsmohmans <mohmans@pm.me>
2022-08-22 18:51:26 +0300

    add articleList component

---
+++ components/Author/ArticlesList.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+      <template v-for="article in articles">
+        <articlePreview
+          :id="article.id"
+          :key="article.id"
+          :article="article"
+        />
+      </template>
+    </div>
+</template>
+
+<script>
+export default {
+    components: {
+        articlePreview: () => import("@/components/Blog/ArticlePreview.vue"),
+    },
+    props: {
+        articles: {
+            type: Array,
+            required: true
+        }
+    },
+}
+</script>
+
+<style scoped>
+
+</style>
